da9270cd515df3299f453a53f81fd4636e9691e9
AgniaStaravoitava <78143065+AgniaStaravoitava@users.noreply.github.com>
2021-05-20 10:39:41 +0300

    feat: task 4 - 1 part

--- 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,12 +10,15 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
-        static void Main(string[] args)
+        private const int TaskAmount = 10;
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
             Console.WriteLine("Each thread should be with the same body and receive a state with integer number, decrement it, print and pass as a state into the newly created thread.");
@@ -26,9 +29,25 @@ namespace MultiThreading.Task4.Threads.Join
 
             Console.WriteLine();
 
-            // feel free to add your code
+            Console.WriteLine("- a) Option");
+
+            var state = 100;
+            for (var taskNumber = 0; taskNumber < TaskAmount; taskNumber++)
+            {
+                var thread = new Thread(() => Output(taskNumber, ref state));
+                thread.Start();
+                thread.Join();
+            }
+            
+            Console.WriteLine("- b) Option");
+
 
             Console.ReadLine();
+        }
+
+        private static void Output(int taskNumber, ref int state)
+        {
+            Console.WriteLine($"Task #{taskNumber} – {state--}");
         }
     }
 }
