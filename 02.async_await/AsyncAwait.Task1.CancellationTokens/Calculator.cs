ee8e7249a510b9e645c325a8b1a52b7eba87522e
AgniaStaravoitava <78143065+AgniaStaravoitava@users.noreply.github.com>
2021-05-28 19:59:57 +0300

    refactor: add files

---
+++ 02.async_await/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncAwait.Task1.CancellationTokens
+{
+    static class Calculator
+    {
+        public static long Calculate(int n, CancellationToken token)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                token.ThrowIfCancellationRequested();
+                //if (token.IsCancellationRequested)
+                //{
+                //    Console.WriteLine("Cancellation requested in token");
+                //    token.ThrowIfCancellationRequested();
+                //}
+                //else 
+                    sum += (i + 1);
+                Thread.Sleep(50);
+            }
+
+            return sum;
+        }
+    }
+}
