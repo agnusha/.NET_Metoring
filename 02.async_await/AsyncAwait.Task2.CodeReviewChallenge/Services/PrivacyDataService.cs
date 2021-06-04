8db51cd0295995440973fce10b4a41fda1ebec85
AgniaStaravoitava <78143065+AgniaStaravoitava@users.noreply.github.com>
2021-06-04 10:25:45 +0300

    feat: change PrivacyDataService

--- 02.async_await/AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
+++ 02.async_await/AsyncAwait.Task2.CodeReviewChallenge/Services/PrivacyDataService.cs
@@ -4,10 +4,13 @@ namespace AsyncAwait.Task2.CodeReviewChallenge.Services
 {
     public class PrivacyDataService : IPrivacyDataService
     {
-        public Task<string> GetPrivacyDataAsync()
+        /*ValueTask is a struct
+         remove AsTask
+         more memory  */
+        public async Task<string> GetPrivacyDataAsync()
         {
-            return new ValueTask<string>("This Policy describes how async/await processes your personal data," +
-                                            "but it may not address all possible data processing scenarios.").AsTask();
+            return await new ValueTask<string>("This Policy describes how async/await processes your personal data," +
+                                            "but it may not address all possible data processing scenarios.");
         }
     }
 }
