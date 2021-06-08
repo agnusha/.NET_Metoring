43294f2ddee37ae5b49d73f47f02a9d0d81e7c3b
AgniaStaravoitava <78143065+AgniaStaravoitava@users.noreply.github.com>
2021-06-08 11:50:18 +0300

    feat: init

---
+++ Messaging_Desktop/Messaging_Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Messaging_Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
