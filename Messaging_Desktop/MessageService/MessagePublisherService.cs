14be055665b8887fc4f7d528972dca88009bca80
AgniaStaravoitava <78143065+AgniaStaravoitava@users.noreply.github.com>
2021-06-10 15:49:01 +0300

    feat: read secrets

--- Messaging_Desktop/MessageService/MessagePublisherService.cs
+++ Messaging_Desktop/MessageService/MessagePublisherService.cs
@@ -1,25 +1,20 @@
 ﻿using Amazon.Runtime;
 using Amazon.SimpleNotificationService;
 using MessageService.Models;
-using Microsoft.Extensions.Options;
-using System;
 using System.Threading.Tasks;
 
 namespace MessageService
 {
     public class MessagePublisherService
     {
-        private readonly ConfigAws _configAws;
         private readonly Publisher _publisher;
 
-        public MessagePublisherService(IOptions<ConfigAws> configAws)
+        public MessagePublisherService(ConfigAws configAws)
         {
-            _configAws = configAws.Value ?? throw new ArgumentNullException(nameof(configAws));
-
-            var credentials = new BasicAWSCredentials(_configAws.AccessKey, _configAws.SecretKey);
+            var credentials = new BasicAWSCredentials(configAws.AccessKey, configAws.SecretKey);
 
             using var snsClient = new AmazonSimpleNotificationServiceClient(credentials);
-            _publisher = new Publisher(snsClient, _configAws.TopicName);
+            _publisher = new Publisher(snsClient, configAws.TopicName);
         }
 
         public async Task SendMessageAsync(string message)
--- Messaging_Desktop/Messaging_Desktop/Program.cs
+++ Messaging_Desktop/Messaging_Desktop/Program.cs
@@ -1,17 +1,26 @@
 ﻿using Example.PublisherApplication;
 using MessageService;
 using System;
+using Microsoft.Extensions.Configuration;
+using MessageService.Models;
 
 namespace Messaging_Desktop
 {
     class Program
     {
-        static void Main(string[] args)
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static void Main()
         {
-            Console.WriteLine("Write a directory to linsten");
+            Console.WriteLine("Write a directory to listen");
             var directory = Console.ReadLine();
 
-            var messagePublisherService = new MessagePublisherService();
+            var configuration = new ConfigurationBuilder()
+              .AddUserSecrets<ConfigAws>()
+              .Build();
+
+            var messagePublisherService = new MessagePublisherService(
+                configuration.GetSection(nameof(ConfigAws)).Get<ConfigAws>());
 
             Watcher.CreateWatcher(directory);
 
