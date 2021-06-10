f72fb43340949dd3edde084bddea66397ebfad83
AgniaStaravoitava <78143065+AgniaStaravoitava@users.noreply.github.com>
2021-06-10 12:22:47 +0300

    feat: config

--- Messaging_Desktop/MessageService/MessagePublisherService.cs
+++ Messaging_Desktop/MessageService/MessagePublisherService.cs
@@ -1,19 +1,25 @@
 ﻿using Amazon.Runtime;
 using Amazon.SimpleNotificationService;
+using MessageService.Models;
+using Microsoft.Extensions.Options;
+using System;
 using System.Threading.Tasks;
 
 namespace MessageService
 {
     public class MessagePublisherService
     {
+        private readonly ConfigAws _configAws;
         private readonly Publisher _publisher;
 
-        public MessagePublisherService()
+        public MessagePublisherService(IOptions<ConfigAws> configAws)
         {
-            var credentials = new BasicAWSCredentials("accessKey", "secretKey");
+            _configAws = configAws.Value ?? throw new ArgumentNullException(nameof(configAws));
+
+            var credentials = new BasicAWSCredentials(_configAws.AccessKey, _configAws.SecretKey);
 
             using var snsClient = new AmazonSimpleNotificationServiceClient(credentials);
-            _publisher = new Publisher(snsClient, "topicName");
+            _publisher = new Publisher(snsClient, _configAws.TopicName);
         }
 
         public async Task SendMessageAsync(string message)
---
+++ Messaging_Desktop/MessageService/Models/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace MessageService.Models
+{
+    public class ConfigAws
+    {
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        public string TopicName { get; set; }
+        public string QueueName { get; set; }
+
+    }
+}
