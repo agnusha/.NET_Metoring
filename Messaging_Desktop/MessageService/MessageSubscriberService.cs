7bee9f40c58768789637dd3ed23c11bd21b3ec38
AgniaStaravoitava <78143065+AgniaStaravoitava@users.noreply.github.com>
2021-06-10 10:38:53 +0300

    feat: add MessageSubscriberService

---
+++ Messaging_Desktop/MessageService/MessageSubscriberService.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using System;
+using System.Threading.Tasks;
+
+namespace MessageService
+{
+    public class MessageSubscriberService
+    {
+        private readonly Subscriber _subscriber;
+
+        public MessageSubscriberService()
+        {
+            var credentials = new BasicAWSCredentials("accessKey", "secretKey");
+
+            using var sqsClient = new AmazonSQSClient(credentials);
+            using var snsClient = new AmazonSimpleNotificationServiceClient(credentials);
+            _subscriber = new Subscriber(sqsClient, snsClient, "topicName", "queueName");
+        }
+
+        public async Task ListenMessageAsync()
+        {
+            await _subscriber.ListenAsync(message => {
+                Console.WriteLine("Received message: " + message);
+            });
+        }
+    }
+}
