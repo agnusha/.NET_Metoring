7146081cdc0d5a2fd0d926351a26b4474eb84f03
AgniaStaravoitava <agnia2000@inbox.ru>
2021-06-26 01:24:47 +0300

    feat: finish task1

--- AppAnalyzing/AppAnalyzing/Program.cs
+++ AppAnalyzing/AppAnalyzing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
 
 namespace AppAnalyzing
@@ -8,13 +9,14 @@ namespace AppAnalyzing
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            GeneratePasswordHashUsingSalt("text", new byte[] { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1 });
+            GeneratePasswordHashUsingSaltQuicker("text", new byte[]{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1 });
         }
 
-        public string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
         {
-
             var iterate = 10000;
-
 
             var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
 
@@ -24,6 +26,24 @@ namespace AppAnalyzing
             Array.Copy(salt, 0, hashBytes, 0, 16); 
             
             Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+
+            return passwordHash;
+        }
+
+        public static string GeneratePasswordHashUsingSaltQuicker(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            var hashBytes = new byte[36];
+
+            salt.CopyTo(hashBytes, 0);
+            hash.CopyTo(hashBytes, 16);
 
             var passwordHash = Convert.ToBase64String(hashBytes);
 
