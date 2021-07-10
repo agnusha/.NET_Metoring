7986b62cbd7b0b85daf80bce7dd5648f630c750e
AgniaStaravoitava <agnia2000@inbox.ru>
2021-07-10 21:52:20 +0300

    feat: MessageSubscriberService init

--- Messaging_Desktop/Messaging_Client/Program.cs
+++ Messaging_Desktop/Messaging_Client/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using MessageService.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using MessageService;
+using System.Threading.Tasks;
 
 namespace Messaging_Client
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Will listen the queue.");
+
+                var configuration = new ConfigurationBuilder()
+                    .AddUserSecrets<ConfigAws>()
+                    .Build();
+                var messageSubscriberService = new MessageSubscriberService(
+                    configuration.GetSection(nameof(ConfigAws)).Get<ConfigAws>());
+
+                await messageSubscriberService.ListenMessageAsync();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
