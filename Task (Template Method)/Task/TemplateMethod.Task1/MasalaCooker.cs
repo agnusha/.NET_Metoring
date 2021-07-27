5c656e580663275e5cd8b6fb5d763ae329c0d3a9
AgniaStaravoitava <agnia2000@inbox.ru>
2021-07-28 01:36:43 +0300

    feat: finish task 1

--- Task (Template Method)/Task/TemplateMethod.Task1/MasalaCooker.cs
+++ Task (Template Method)/Task/TemplateMethod.Task1/MasalaCooker.cs
@@ -13,7 +13,30 @@ namespace TemplateMethod.Task1
 
         public void CookMasala(Country country)
         {
-            throw new NotImplementedException();
+            if (country == Country.India)
+            {
+                cooker.FryChicken(100, Level.Strong);
+                cooker.FryRice(200, Level.Strong);
+                cooker.PrepareTea(15, TeaKind.Green, 12);
+
+                cooker.PepperChicken(Level.Strong);
+                cooker.PepperRice(Level.Strong);
+
+                cooker.SaltChicken(Level.Strong);
+                cooker.SaltRice(Level.Strong);
+            }
+            else if (country == Country.Ukraine)
+            {
+                cooker.FryChicken(300, Level.Medium);
+                cooker.FryRice(500, Level.Strong);
+                cooker.PrepareTea(10, TeaKind.Black, 10);
+
+                cooker.PepperChicken(Level.Low);
+                cooker.PepperRice(Level.Low);
+
+                cooker.SaltChicken(Level.Medium);
+                cooker.SaltRice(Level.Strong);
+            }
         }
     }
 }
